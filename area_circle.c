5d0832a5cbad505c68699342a946e6cdbca75b54
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-11-03 05:34:45 +0530

    Crear area_circle_volume_cylinder.c

---
+++ area_circle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+  int radius, height;
+  printf("Enter the radius of circle:");
+  scanf("%d", &radius);
+  printf("The area of circle is %f\n", 3.14 * radius * radius);
+  printf("Enter the height of cylinder:");
+    scanf("%d", &height);
+  printf("The volume of cylider is%f", 3.14 * radius * radius * height);
+  return 0;
+}
