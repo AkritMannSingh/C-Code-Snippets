760669842ff475da72ba8f14f0a3d1b0c0c59b8d
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-12-21 12:40:26 +0530

    Update year_verification.c

--- year_verification.c
+++ year_verification.c
@@ -9,7 +9,7 @@ int main()
 	printf("Enter the Year:");
 	scanf("%d", &year);
 
-	if (year % 4 == 0 && year && 100 != 0 || year % 400 == 0)
+	if (year % 4 == 0 && 100 != 0 || year % 400 == 0)
 	{
 		printf("Hey! This is Leap year..\n");
 	}
